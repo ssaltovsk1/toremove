3e1aea48b1e0acafae2d2ca59d8af4b64141d488
ssaltovsk1 <ssaltovsk1@gmail.com>
2022-11-08 11:50:58 +0300

    first - second try

--- src/main/java/Just/toremove.java
+++ src/main/java/Just/toremove.java
@@ -2,6 +2,6 @@ package Just;
 
 public class toremove {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        System.out.println("By"); //this were changed
     }
 }
